6403f14aa2a2d56ab1a36e3b8f651bd23796f0c1
simran019 <simran.19.1677@gmail.com>
2023-10-29 15:53:54 +0530

    added headercartbutton functionality

--- app/HeaderCartButton/page.tsx
+++ app/HeaderCartButton/page.tsx
@@ -1,8 +1,13 @@
+import { useContext } from "react";
 import CartIcon from "../../public/Svg/cartIcon.svg";
 import Image from "next/image";
+import CartContext from "../store/cartContext";
 
 const HeaderCartButton = (props:any) => {
-  
+    const cartCtx = useContext(CartContext);
+    const noOfItems = cartCtx.items.reduce((totalNo,item:any)=>{
+      return totalNo + item.quantity
+    },0)
     const clickHandler =()=>{
         props.onShow(true)
     }
@@ -11,7 +16,7 @@ const HeaderCartButton = (props:any) => {
     <button className="flex items-center justify-between w-52 px-4 py-2 rounded-full bg-red-600" onClick={clickHandler}>
       <Image src={CartIcon} width={50} height={50} alt="cart_icon_image" />
       <h6>My Cart</h6>
-      <div className="bg-red-800 p-2 rounded-xl">0</div>
+      <div className="bg-red-800 p-2 rounded-xl">{noOfItems}</div>
     </button>
   );
 };
